34637927e0c620ce22e18f74a5d7f2faac14a9ce
MaysaraHayajneh <maysara.hayajneh12@gmail.com>
2026-06-02 21:43:01 +0300

    Add project files.

---
+++ RabbimqConsumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+await ConumeMessages();
+async Task ConumeMessages()
+{
+	var factory = new ConnectionFactory() { HostName = "localhost" };
+	using (var connection = await factory.CreateConnectionAsync())
+	{
+		using (var channel = await connection.CreateChannelAsync())
+		{
+			var consumer = new AsyncEventingBasicConsumer(channel);
+			consumer.ReceivedAsync += async (model, ea) =>
+			{
+				var body = ea.Body.ToArray();
+				var message = Encoding.UTF8.GetString(body);
+				Console.WriteLine($"{message} has been cosumed by {ea.ConsumerTag}");
+				Console.WriteLine($"===================================================");
+				await channel.BasicAckAsync(
+					deliveryTag: ea.DeliveryTag,
+					multiple: false
+					);
+			};
+
+			await channel.BasicConsumeAsync(
+				queue: "loginfo",
+				autoAck: false,
+				consumer: consumer,
+				consumerTag: "info"
+				);
+			await channel.BasicConsumeAsync(
+				queue: "logerror",
+				autoAck: false,
+				consumer: consumer,
+				consumerTag: "error"
+				);
+			await channel.BasicConsumeAsync(
+				queue: "logall",
+				autoAck: false,
+				consumer: consumer,
+				consumerTag: "all"
+				);
+
+
+			Console.WriteLine("press enter to exit");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ RabbitMqDemoConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+
+await Generate_Log_TopicExchange();
+async Task Generate_Log_TopicExchange()
+{
+	var factory = new ConnectionFactory() { HostName = "localhost" };
+	using var connection = await factory.CreateConnectionAsync();
+
+	using (var channel = await connection.CreateChannelAsync())
+	{
+		for (int i = 0; i < 1000; i++)
+		{
+			if (i % 2 == 0)
+			{
+				Thread.Sleep(2000);
+				var message = $"log info {i}";
+				var body = Encoding.UTF8.GetBytes(message);
+
+				await channel.BasicPublishAsync(
+					exchange: "amq.topic",
+					routingKey: "log.info",
+					body: body
+					);
+			}
+			else
+			{
+				Thread.Sleep(2000);
+				var message = $"log info {i}";
+				var body = Encoding.UTF8.GetBytes(message);
+
+				await channel.BasicPublishAsync(
+					exchange: "amq.topic",
+					routingKey: "log.error",
+					body: body
+					);
+			}
+
+		}
+
+		Console.WriteLine("press enter to exist");
+		Console.ReadLine();
+	}
+	;
+}
