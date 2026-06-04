1280dac365e2f82c10b1d2571589d1900cfa3901
Maysara.Hayajneh <Maysara.Hayajneh@MAYSARA-H>
2026-06-04 15:02:32 +0300

    d

---
+++ ReliableExchangeRabbitMq/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using RabbitMQ.Client;
+await Publish();
+async Task Publish()
+{
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+
+    using (var connection = await factory.CreateConnectionAsync())
+    {
+        using (var channel = await connection.CreateChannelAsync())
+        {
+            channel.BasicNacksAsync += async (sender, ea) =>
+             {
+                 Console.WriteLine($"Message with delivery tag {ea.DeliveryTag} has been acknowledged.");
+                 await Task.CompletedTask;
+
+             };
+
+
+            channel.BasicReturnAsync += async (sender, ea) =>
+            {
+
+                Console.WriteLine($"Message with delivery tag {ea.Exchange} has been returned.");
+                await Task.CompletedTask;
+
+            };
+            string msg = "hellow world";
+
+            await channel.BasicPublishAsync(
+                exchange: "reliable-exchange",
+                routingKey: "reliable-routing-key",
+                body: System.Text.Encoding.UTF8.GetBytes(msg),
+                mandatory: true
+                );
+
+        }
+    }
+
+}
+
+Console.ReadLine();
