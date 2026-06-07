e8c5da29a9e54c4e325e96a5417e565b4dfa5a21
Maysara.Hayajneh <Maysara.Hayajneh@MAYSARA-H>
2026-06-07 15:14:14 +0300

    stream

--- RabbimqConsumer/Program.cs
+++ RabbimqConsumer/Program.cs
@@ -1,196 +1,236 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
-using System.Runtime.CompilerServices;
 using System.Text;
-await ConumeMessages_dead_letter_exchange_routing();
+await ConumeMessages_topic_permission_read();
 async Task ConumeMessages()
 {
-	var factory = new ConnectionFactory() { HostName = "localhost" };
-	using (var connection = await factory.CreateConnectionAsync())
-	{
-		using (var channel = await connection.CreateChannelAsync())
-		{
-			var consumer = new AsyncEventingBasicConsumer(channel);
-			consumer.ReceivedAsync += async (model, ea) =>
-			{
-				var body = ea.Body.ToArray();
-				var message = Encoding.UTF8.GetString(body);
-				Console.WriteLine($"{message} has been cosumed by {ea.ConsumerTag}");
-				Console.WriteLine($"===================================================");
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    using (var connection = await factory.CreateConnectionAsync())
+    {
+        using (var channel = await connection.CreateChannelAsync())
+        {
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"{message} has been cosumed by {ea.ConsumerTag}");
+                Console.WriteLine($"===================================================");
 
-				await channel.BasicAckAsync(  // acknowledgment is a mechanism used in message queuing systems to confirm that a message has been successfully received and processed by a consumer
-											  // . It helps ensure reliable message delivery and allow // s the message broker to manage the lifecycle of messages effectively (deleted/deleting it).
+                await channel.BasicAckAsync(  // acknowledgment is a mechanism used in message queuing systems to confirm that a message has been successfully received and processed by a consumer
+                                              // . It helps ensure reliable message delivery and allow // s the message broker to manage the lifecycle of messages effectively (deleted/deleting it).
                     deliveryTag: ea.DeliveryTag,
-					multiple: false
-					);
-				
+                    multiple: false
+                    );
 
 
-				await channel.BasicNackAsync(   // This means i do negative acknowledgment to the message ,  determine if requeue or not  //
+
+                await channel.BasicNackAsync(   // This means i do negative acknowledgment to the message ,  determine if requeue or not  //
                                                 // this will activate the deae-letter exchange freature if teh requeue is false and the message will be sent to the dead-letter exchange if it is configured, otherwise it will be discarded.
 
                     deliveryTag: ea.DeliveryTag,
-					multiple: false,
-					requeue: false
+                    multiple: false,
+                    requeue: false
                     );
-			};
+            };
 
-			await channel.BasicConsumeAsync(
-				queue: "loginfo",
-				autoAck: false, // has three types of acknowledgment: autoAck, manualAck, and NAck (negative acknowledgment)
+            await channel.BasicConsumeAsync(
+                queue: "loginfo",
+                autoAck: false, // has three types of acknowledgment: autoAck, manualAck, and NAck (negative acknowledgment)
                 consumer: consumer,
-				consumerTag: "info"
-				);
-			await channel.BasicConsumeAsync(
-				queue: "logerror",
-				autoAck: false, // has three types of acknowledgment: autoAck, manualAck, and NAck (negative acknowledgment)
+                consumerTag: "info"
+                );
+            await channel.BasicConsumeAsync(
+                queue: "logerror",
+                autoAck: false, // has three types of acknowledgment: autoAck, manualAck, and NAck (negative acknowledgment)
                 consumer: consumer,
-				consumerTag: "error"
-				);
-			await channel.BasicConsumeAsync(
-				queue: "logall",
-				autoAck: false, // has three types of acknowledgment: autoAck, manualAck, and NAck (negative acknowledgment)
+                consumerTag: "error"
+                );
+            await channel.BasicConsumeAsync(
+                queue: "logall",
+                autoAck: false, // has three types of acknowledgment: autoAck, manualAck, and NAck (negative acknowledgment)
                 consumer: consumer,
-				consumerTag: "all"
-				);
-			 
+                consumerTag: "all"
+                );
 
-			Console.WriteLine("press enter to exit");
-			Console.ReadKey();
-		}
-	}
+
+            Console.WriteLine("press enter to exit");
+            Console.ReadKey();
+        }
+    }
 }
 async Task ConumeMessages_auto_deleted()
 {
-	var factory = new ConnectionFactory() { HostName = "localhost" };
-	using (var connection = await factory.CreateConnectionAsync())
-	{
-		using (var channel = await connection.CreateChannelAsync())
-		{
-			var consumer = new AsyncEventingBasicConsumer(channel);
-			consumer.ReceivedAsync += async (model, ea) =>
-			{
-				var body = ea.Body.ToArray();
-				var message = Encoding.UTF8.GetString(body);
-				Console.WriteLine($"{message} has been cosumed by {ea.ConsumerTag}");
-				Console.WriteLine($"===================================================");
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    using (var connection = await factory.CreateConnectionAsync())
+    {
+        using (var channel = await connection.CreateChannelAsync())
+        {
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"{message} has been cosumed by {ea.ConsumerTag}");
+                Console.WriteLine($"===================================================");
 
-				await channel.BasicAckAsync(
-					deliveryTag: ea.DeliveryTag,
-					multiple: false
-					);
-			};
+                await channel.BasicAckAsync(
+                    deliveryTag: ea.DeliveryTag,
+                    multiple: false
+                    );
+            };
 
-			await channel.BasicConsumeAsync(
-				queue: "autodeleted-queue",
-				autoAck: false,
-				consumer: consumer
-				);
+            await channel.BasicConsumeAsync(
+                queue: "autodeleted-queue",
+                autoAck: false,
+                consumer: consumer
+                );
 
 
-			Console.WriteLine("press enter to exit");
-			Console.ReadKey();
-		}
-	}
+            Console.WriteLine("press enter to exit");
+            Console.ReadKey();
+        }
+    }
 }
 async Task ConumeMessages_expierd()
 {
-	var factory = new ConnectionFactory() { HostName = "localhost" };
-	using (var connection = await factory.CreateConnectionAsync())
-	{
-		using (var channel = await connection.CreateChannelAsync())
-		{
-			var consumer = new AsyncEventingBasicConsumer(channel);
-			consumer.ReceivedAsync += async (model, ea) =>
-			{
-				var body = ea.Body.ToArray();
-				var message = Encoding.UTF8.GetString(body);
-				Console.WriteLine($"{message} has been cosumed by {ea.ConsumerTag}");
-				Console.WriteLine($"===================================================");
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    using (var connection = await factory.CreateConnectionAsync())
+    {
+        using (var channel = await connection.CreateChannelAsync())
+        {
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"{message} has been cosumed by {ea.ConsumerTag}");
+                Console.WriteLine($"===================================================");
 
-				await channel.BasicAckAsync(
-					deliveryTag: ea.DeliveryTag,
-					multiple: false
-					);
-			};
+                await channel.BasicAckAsync(
+                    deliveryTag: ea.DeliveryTag,
+                    multiple: false
+                    );
+            };
 
-			await channel.BasicConsumeAsync(
-				queue: "expired-queue",
-				autoAck: false,
-				consumer: consumer
-				);
+            await channel.BasicConsumeAsync(
+                queue: "expired-queue",
+                autoAck: false,
+                consumer: consumer
+                );
 
 
-			Console.WriteLine("press enter to exit");
-			Console.ReadKey();
-		}
-	}
+            Console.WriteLine("press enter to exit");
+            Console.ReadKey();
+        }
+    }
 }
 async Task ConumeMessages_message_ttl()
 {
-	var factory = new ConnectionFactory() { HostName = "localhost" };
-	using (var connection = await factory.CreateConnectionAsync())
-	{
-		using (var channel = await connection.CreateChannelAsync())
-		{
-			var consumer = new AsyncEventingBasicConsumer(channel);
-			consumer.ReceivedAsync += async (model, ea) =>
-			{
-				var body = ea.Body.ToArray();
-				var message = Encoding.UTF8.GetString(body);
-				Console.WriteLine($"{message} has been cosumed by {ea.ConsumerTag}");
-				Console.WriteLine($"===================================================");
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    using (var connection = await factory.CreateConnectionAsync())
+    {
+        using (var channel = await connection.CreateChannelAsync())
+        {
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"{message} has been cosumed by {ea.ConsumerTag}");
+                Console.WriteLine($"===================================================");
 
-				await channel.BasicAckAsync(
-					deliveryTag: ea.DeliveryTag,
-					multiple: false
-					);
-			};
+                await channel.BasicAckAsync(
+                    deliveryTag: ea.DeliveryTag,
+                    multiple: false
+                    );
+            };
 
-			await channel.BasicConsumeAsync(
-				queue: "message-ttl-queue",
-				autoAck: false,
-				consumer: consumer
-				);
+            await channel.BasicConsumeAsync(
+                queue: "message-ttl-queue",
+                autoAck: false,
+                consumer: consumer
+                );
 
 
-			Console.WriteLine("press enter to exit");
-			Console.ReadKey();
-		}
-	}
+            Console.WriteLine("press enter to exit");
+            Console.ReadKey();
+        }
+    }
 }
 async Task ConumeMessages_dead_letter_exchange_routing()
 {
-	var factory = new ConnectionFactory() { HostName = "localhost" };
+    var factory = new ConnectionFactory() { HostName = "localhost" };
 
-	var connection = await factory.CreateConnectionAsync();
-	var channel = await connection.CreateChannelAsync();
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
 
-	var consumer = new AsyncEventingBasicConsumer(channel);
+    var consumer = new AsyncEventingBasicConsumer(channel);
 
-	consumer.ReceivedAsync += async (model, ea) =>
-	{
-		var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    consumer.ReceivedAsync += async (model, ea) =>
+    {
+        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
 
-		Console.WriteLine($"{message} received");
+        Console.WriteLine($"{message} received");
 
-		await channel.BasicRejectAsync(
-			deliveryTag: ea.DeliveryTag,
-			requeue: false
-		);
+        await channel.BasicRejectAsync(
+            deliveryTag: ea.DeliveryTag,
+            requeue: false
+        );
 
-		Console.WriteLine($"{message} rejected → DLX");
-	};
+        Console.WriteLine($"{message} rejected → DLX");
+    };
 
-	await channel.BasicConsumeAsync(
-		queue: "message-dead-queue",
-		autoAck: false,
-		consumer: consumer
-	);
+    await channel.BasicConsumeAsync(
+        queue: "message-dead-queue",
+        autoAck: false,
+        consumer: consumer
+    );
 
-	Console.WriteLine("Consumer running... press ENTER to exit");
-	Console.ReadLine();
+    Console.WriteLine("Consumer running... press ENTER to exit");
+    Console.ReadLine();
 
-	await channel.CloseAsync();
-	await connection.CloseAsync();
+    await channel.CloseAsync();
+    await connection.CloseAsync();
+}
+async Task ConumeMessages_topic_permission_read()
+{
+    var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "dev", UserName = "maysara", Password = "maysara" };
+
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    await channel.QueueBindAsync(
+        queue: "q1",
+        exchange: "amq.topic",
+        routingKey: "log.error"
+    );
+
+    var consumer = new AsyncEventingBasicConsumer(channel);
+
+    consumer.ReceivedAsync += async (model, ea) =>
+    {
+        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+        Console.WriteLine($"{message} received");
+
+        await channel.BasicAckAsync(
+            deliveryTag: ea.DeliveryTag,
+            multiple: false
+        );
+
+        Console.WriteLine($"{message} acknowledged");
+    };
+
+    await channel.BasicConsumeAsync(
+        queue: "q1",
+        autoAck: false,
+        consumer: consumer
+    );
+
+    Console.WriteLine("Consumer running... press ENTER to exit");
+    Console.ReadLine();
+
+    await channel.CloseAsync();
+    await connection.CloseAsync();
 }
---
+++ RabbitMqStream/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+
+using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.Reliable;
+using System.Buffers;
+using System.Net;
+using System.Text;
+
+await ReadFromStream();
+
+async Task ReadFromStream()
+{
+
+
+    var config = new StreamSystemConfig()
+    {
+        VirtualHost = "dev",
+        Endpoints = new List<EndPoint> { new IPEndPoint(IPAddress.Loopback, 5552) }, // rabbit ,q stream port
+        UserName = "maysara",
+        Password = "maysara"
+    };
+
+
+    var streamSystem = await StreamSystem.Create(config);
+
+    const string streamName = "stream-q";
+
+    var consumerConfig = new ConsumerConfig(streamSystem, streamName)
+    {
+        Reference = "DOTNET 10",
+
+        //OffsetSpec = new OffsetTypeOffset(), if this did not get determinde will read from the next by default
+
+
+
+        // recive teh message event 
+        MessageHandler = async (sourceStream, consumer, ctx, message) =>
+        {
+            Console.WriteLine($"message coming form {sourceStream} daata :{Encoding.Default.GetString(message.Data.Contents.ToArray())}  consumed {consumer.Info.Identifier}");
+        }
+    };
+
+    var consumer = await Consumer.Create(consumerConfig);
+
+    Console.WriteLine("Consumer created, now subscribing to stream");
+    Console.ReadLine();
+
+    await consumer.Close();
+    await streamSystem.Close();
+
+}
